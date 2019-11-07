221d2791f1dfe4ea3cac79bec4f0b176640fd5dd
icompsofteduardo <eduardo.chaves@icompsoft.com.br>
2019-11-07 14:38:03 -0300

    terminando curso js

--- js-avancado/aluraframe/client/js/app/services/HttpService.js
+++ js-avancado/aluraframe/client/js/app/services/HttpService.js
@@ -1,34 +1,27 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if (res.ok) {
+            return res;
+        } else {
+            throw new Error(res.statusText);
+        }
+    }
+
     get(url) {
         return fetch(url)
+            .then(res => this._handleErrors(res))
             .then(res => res.json());
 
     }
 
     post(url, dado) {
 
-
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-        });
-
+        return fetch(url, {
+            headers: { 'Content-type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
     }
 }
